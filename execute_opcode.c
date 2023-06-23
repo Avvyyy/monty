19aa4b8c8e5d3c25502b673e5e110a712f6e0994
Favour A. Uzochukwu <116796967+Avvyyy@users.noreply.github.com>
2023-06-23 05:46:26 -0700

    Update execute_opcode.c

--- execute_opcode.c
+++ execute_opcode.c
@@ -1,32 +1,38 @@
 #include "monty.h"
 
+/* Define an array of instructions */
+instruction_t instructions[] = {
+	{"push", push},
+	{"pall", pall},
+	{"pint", pint},
+	{"pop", pop},
+	{"swap", swap},
+	{"add", add},
+	{"nop", nop},
+	{NULL, NULL} /* Last element of the array should be NULL for termination */
+};
+
 /**
  * execute_opcode - Execute the corresponding opcode function based on input
  * @stack: Double pointer to the stack
  * @opcode: The opcode to execute
  * @line_number: The current line number in the bytecode file
  */
-
-void execute_opcode(stack_t **current_stack, char *opcode, unsigned int line_number)
+void execute_opcode(stack_t **stack, char *opcode, unsigned int line_number)
 {
-	if (strcmp(opcode, "push") == 0)
-		push(stack, line_number);
-	else if (strcmp(opcode, "pall") == 0)
-		pall(stack, line_number);
-	else if (strcmp(opcode, "pint") == 0)
-		pint(stack, line_number);
-	else if (strcmp(opcode, "pop") == 0)
-		pop(stack, line_number);
-	else if (strcmp(opcode, "swap") == 0)
-		swap(stack, line_number);
-	else if (strcmp(opcode, "add") == 0)
-		add(stack, line_number);
-	else if (strcmp(opcode, "nop") == 0)
-		nop(stack, line_number);
-	else
+	int i = 0;
+
+	while (instructions[i].opcode != NULL)
 	{
-		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-		free_stack(stack);
-		exit(EXIT_FAILURE);
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(stack, line_number);
+			return;
+		}
+		i++;
 	}
+
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	free_stack(stack);
+	exit(EXIT_FAILURE);
 }
