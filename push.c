64b1b474c48246a0978fdb78593820d7fe8fbe72
Kobby <95kobby@gmail.com>
2023-06-23 18:02:16 -0700

    Extra commits

--- push.c
+++ push.c
@@ -48,24 +48,3 @@ void s_push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
-
-/**
- * s_pall - prints all the values on the stack,
- * starting from the top of the stack
- * @stack: head
- * @line_number: current line number
- */
-void s_pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp;
-
-	temp = *stack;
-	if (temp == NULL)
-		return;
-	if (line_number)
-		while (temp)
-		{
-			printf("%d\n", temp->n);
-			temp = temp->next;
-		}
-}
--- queue.c
+++ queue.c
@@ -10,15 +10,3 @@ void s_queue(stack_t **stack, unsigned int line_number)
 	if (stack || line_number)
 		global.flag = 0;
 }
-
-/**
- * s_stack - sets the format of the data to a stack (LIFO)
- * @stack:pointer to head
- * @line_number: num line
- */
-
-void s_stack(stack_t **stack, unsigned int line_number)
-{
-	if (stack || line_number)
-		global.flag = 1;
-}
